a58aded5eb5eb22ce10e5086c35c0f5b5bb2d524
Mogrima <rebel.rs@yandex.ru>
2024-03-12 23:17:25 +0500

    На все объекты в игре будет действовать сила тяжести.

--- src/Game.js
+++ src/Game.js
@@ -7,6 +7,8 @@ export class Game {
         this.width = this.canvas.width;
         this.height = this.canvas.height;
         this.player = new Player(this);
+        // сила тяжести в 1px на каждый кадр анимации
+        this.gravity = 1;
         this.baseHeight = 720;
         this.ratio = this.height / this.baseHeight;
         this.resize(window.innerWidth, window.innerHeight);
