747d708078d34516c791294fd1f73d8e9c737a93
Mogrima <rebel.rs@yandex.ru>
2024-03-12 22:39:30 +0500

    Добавлен прослушиватель событий изменения размера окна в конструктор класса. При смене размера будет запускаться метод resize, который изменяет размеры канваса и свойства ширины и высоты игры.

--- src/Game.js
+++ src/Game.js
@@ -7,11 +7,24 @@ export class Game {
         this.width = this.canvas.width;
         this.height = this.canvas.height;
         this.player = new Player(this);
+        this.resize(window.innerWidth, window.innerHeight);
+
+        window.addEventListener('resize', e => {
+            this.resize(e.currentTarget.innerWidth,
+                e.currentTarget.innerHeight);
+        });
     }
 
     render() {
         this.ctx.fillStyle = 'red';
         this.player.update();
         this.player.draw();
+    }
+
+    resize(width, height) {
+        this.canvas.width = width;
+        this.canvas.height = height;
+        this.width = width;
+        this.height = height;
     }
 }
