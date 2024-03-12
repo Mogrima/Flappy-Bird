82158a87d0f847ef107af80cb58a7c4d2e41e2a2
Mogrima <rebel.rs@yandex.ru>
2024-03-12 21:56:14 +0500

    Создаем экземпляр класса игрока при инициализации класса игры. В игровой класс добавляется метод render, который будет запускать все методы обновления и отрисовки игровых элементов.

--- src/Game.js
+++ src/Game.js
@@ -1,8 +1,17 @@
+import { Player } from "./Player.js";
+
 export class Game {
     constructor(canvas, ctx) {
         this.canvas = canvas;
         this.ctx = ctx;
         this.width = this.canvas.width;
         this.height = this.canvas.height;
+        this.player = new Player(this);
+    }
+
+    render() {
+        this.ctx.fillStyle = 'red';
+        this.player.update();
+        this.player.draw();
     }
 }
