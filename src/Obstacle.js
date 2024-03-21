9335635462fc979f5f01fa78a1622525176fc268
Mogrima <rebel.rs@yandex.ru>
2024-03-21 11:25:16 +0500

    Вместо перевода свойства gameOver в true, срабатывает метод truggerGameOver.

--- src/Obstacle.js
+++ src/Obstacle.js
@@ -35,13 +35,15 @@ export class Obstacle {
             this.game.obstacles = this.game.obstacles.filter(obstacle => 
                 !obstacle.markedForDeletion);
             this.game.score++;
-            if (this.game.obstacles.length <= 0) this.game.gameOver = true;
+            if (this.game.obstacles.length <= 0) {
+                this.game.triggerGameOver();
+            }
         }
 
         if (this.game.checkCollision(this, this.game.player)) {
-            this.game.gameOver = true;
             this.game.player.collided = true;
             this.game.player.stopCharge();
+            this.game.triggerGameOver();
         }
     }
 
