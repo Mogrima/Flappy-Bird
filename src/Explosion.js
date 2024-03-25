e2ad2476ea26e8ccf2b00a20f672285eb7546d30
Mogrima <rebel.rs@yandex.ru>
2024-03-25 23:52:31 +0500

    Создан класс анимации взрыва.

---
+++ src/Explosion.js
@@ -0,0 +1,30 @@
+export class Explosion {
+    constructor(game, x, y) {
+        this.game = game;
+        this.image = document.getElementById('smokeExplosion');
+        this.frameX = 0;
+        this.spriteWidth = 200;
+        this.spriteHeight = 200;
+        this.width = this.spriteWidth * this.game.ratio;
+        this.height = this.spriteHeight * this.game.ratio;
+        this.x = x - this.width * 0.5;
+        this.y = y - this.height * 0.5;
+        this.maxFrame = 8;
+    }
+    update() {
+        this.x -= this.game.speed;
+        if (this.game.eventUpdate) {
+            this.frameX++;
+        } 
+        if (this.frameX > this.maxFrame) this.remove();
+    }
+    draw() {
+        this.game.ctx.drawImage(this.image, this.frameX * this.spriteWidth, 0,
+            this.spriteWidth, this.spriteHeight, this.x, this.y,
+            this.width, this.height);
+    }
+
+    remove() {
+        this.game.particles.delete(this);
+    }
+}
