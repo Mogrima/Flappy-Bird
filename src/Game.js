5a17ca22dae6de2671501a814dcf527a0441d03b
Mogrima <rebel.rs@yandex.ru>
2024-03-26 00:18:29 +0500

    При активации полноэкранного режима на мобильном устройстве, экран переходит в альбомную ориентацию.

--- src/Game.js
+++ src/Game.js
@@ -153,6 +153,7 @@ export class Game {
     toggleFullScreen() {
         if (!document.fullscreenElement) {
             document.documentElement.requestFullscreen();
+            screen.orientation.lock("landscape-primary");
         } else if (document.exitFullscreen) {
             document.exitFullscreen();
         }
